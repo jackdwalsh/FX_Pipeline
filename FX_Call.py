c94d22a6998e9ae20e4307088ce3cb91ed67ade0
Jack D <jackdwalsh@proton.me>
2025-03-06 17:08:46 +1100

    Latest workings 6 Mar 2025

--- FX_Call.py
+++ FX_Call.py
@@ -13,18 +13,25 @@ from_currency = 'USD'
 to_currency   = 'AUD'
 api_key       = os.getenv('API_KEY')
 
-url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey={api_key}'
-r = requests.get(url)
-data = r.json()
+try:
+    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey={api_key}'
+    r = requests.get(url)
+    data = r.json()
+
+    df = pd.DataFrame.from_dict(data['Realtime Currency Exchange Rate'], orient='index', columns=['value'])
+
+    # Reset index to move the index into a column
+    df = df.reset_index()
+
+    # Rename columns
+    df.columns = ['headers', 'value']
+
+    #save to local to test other transformations
+    df.to_pickle(f'df.pkl')    
+
+    print("Success")
+except:
+    print("ERROR with API call, data not returned")
 
 
-df = pd.DataFrame.from_dict(data['Realtime Currency Exchange Rate'], orient='index', columns=['value'])
 
-# Reset index to move the index into a column
-df = df.reset_index()
-
-# Rename columns
-df.columns = ['headers', 'value']
-
-#save to local to test other transformations
-df.to_pickle(f'{localpath}df.pkl')    
